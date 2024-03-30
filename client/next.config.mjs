6cbee02783920805c3f94ff4a6dc38eadcdd4250
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-03-30 19:03:22 +0200

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-       domains: ["alsayedalaa-eg.com"],
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
