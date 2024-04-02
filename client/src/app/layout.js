1da90b381147ca09bdbc1b4b33d3261ec2baabf3
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-04-02 05:04:18 +0200

    Update layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -83,7 +83,7 @@ export default function RootLayout({ children }) {
 <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2XYQVKSP6Q"></Script>
 <Script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-2XYQVKSP6Q');
