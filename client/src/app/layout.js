2c91459c6ec3cfa8397f6b496dbbf1dd7224a2e8
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-04-03 03:14:15 +0200

    Update layout.js

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -6,32 +6,44 @@ import Nav from "./components/home-components/Nav";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Footer from "./components/home-components/Footer";
 import { usePathname } from "next/navigation";
-import Head from "next/head"; // استيراد Head من next/head
 import { useEffect } from "react";
-import { ToastContainer } from "react-toastify";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
+
   useEffect(() => {
     document.title = " مؤسسة السيد علاء لتصنيع ماكينات الرخام والجرانيت والأوناش";
+    const googleTagScript = document.createElement("script");
+    googleTagScript.async = true;
+    googleTagScript.src =
+      "https://www.googletagmanager.com/gtag/js?id=G-2XYQVKSP6Q";
+    document.head.appendChild(googleTagScript);
+
+    const gtagScript = document.createElement("script");
+    gtagScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2XYQVKSP6Q');
+    `;
+    document.head.appendChild(gtagScript);
 
     const metaDescription = document.createElement("meta");
     metaDescription.name = "description";
     metaDescription.content =
-"مؤسسة السيد علاء , السيد علاء , ماكينات الرخام , ماكينات الجرانيت  , ماكينات الرخام , أحدث ماكينات الرخام , مكن رخام , تقطيع رخام , جلاية رخام , منشار رخام, ماكينات رخام ,اسعار ماكينات جلى الرخام,اسعار ماكينة جلي الرخام , اماكن بيع ماكينات الرخام,الات قص الرخام, تصنيع ماكينات الرخام,تصنيع ماكينات الرخام والجرانيت ,ماكينات تقطيع رخام , ماكينة حلية رخام, ماكينة قص رخام, مكن رخام, ماكينة تلميع الرخام للبيع, مكن رخام مستعمل للبيع, تصنيع ماكينات رخام, اسعار منشار رخام ايطالي , جلي رخام , marble machines , marble and granite , marble , granite , أوناش عنبر , أوناش , فرايز , تقطيع جرانيت , تقطيع حجر صناعي , رخام , حجر صناعي "
+      "مؤسسة السيد علاء , السيد علاء , ماكينات الرخام , ماكينات الجرانيت  , ماكينات الرخام , أحدث ماكينات الرخام , مكن رخام , تقطيع رخام , جلاية رخام , منشار رخام , جلي رخام , marble machines , marble and granite , marble , granite , أوناش عنبر , أوناش , فرايز , تقطيع جرانيت , تقطيع حجر صناعي , رخام , حجر صناعي ";
 
     document.head.appendChild(metaDescription);
 
     return () => {
+      document.head.removeChild(googleTagScript);
+      document.head.removeChild(gtagScript);
       document.head.removeChild(metaDescription);
     };
   }, [pathname]);
 
   return (
     <html lang="en">
-      <Head>
-        <title>مؤسسة السيد علاء</title>
-      </Head>
       <body
         className={`position-relative ${
           pathname.split("/")[1] === "admin" ? "admin" : "client"
@@ -54,7 +66,6 @@ export default function RootLayout({ children }) {
             }}
           ></div>
         )}
-
         <div
           style={{ zIndex: 99, minHeight: "100vh" }}
           className="position-relative"
